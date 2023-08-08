cf937be416f2444c19498b3fea70f36f41affae8
mehsen-alk <mehsen2222@gmail.com>
2023-08-08 21:39:05 +0300

    try open url

--- lib/module_landing_page/screen/landing_page_screen.dart
+++ lib/module_landing_page/screen/landing_page_screen.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 @injectable
 class LandingPageScreen extends StatefulWidget {
@@ -14,8 +15,26 @@ class LandingPageScreen extends StatefulWidget {
 class _LandingPageScreenState extends State<LandingPageScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Placeholder(),
+    return Scaffold(
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              var url =
+                  'https://play.google.com/store/apps/details?id=de.yessoft.c4d_captain';
+              var uri = Uri.tryParse(url);
+              if (uri != null) {
+                canLaunchUrl(uri).then((value) {
+                  if (value) {
+                    launchUrl(uri);
+                  }
+                });
+              }
+            },
+            child: Text('press me'),
+          ),
+        ],
+      ),
     );
   }
 }
